1bc3eb8477d7a5b798d6a8d45edb3ae73829c976
PAl-6 <nsd_niit@hotmail.com>
2019-11-08 17:26:04 +0530

    CircuitBraker

--- Applications/TimesheetsServer/Startup.cs
+++ Applications/TimesheetsServer/Startup.cs
@@ -9,6 +9,7 @@ using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 using Timesheets;
 using Pivotal.Discovery.Client;
 using Steeltoe.Common.Discovery;
+using Steeltoe.CircuitBreaker.Hystrix;
 
 namespace TimesheetsServer
 {
@@ -38,8 +39,10 @@ namespace TimesheetsServer
                     BaseAddress = new Uri(Configuration.GetValue<string>("REGISTRATION_SERVER_ENDPOINT"))
                 };
 
-                return new ProjectClient(httpClient);
+                var logger = sp.GetService<ILogger<ProjectClient>>();
+                return new ProjectClient(httpClient, logger);
             });
+            services.AddHystrixMetricsStream(Configuration);
             services.AddDiscoveryClient(Configuration);
         }
 
@@ -51,6 +54,8 @@ namespace TimesheetsServer
 
             app.UseMvc();
             app.UseDiscoveryClient();
+            app.UseHystrixMetricsStream();
+            app.UseHystrixRequestContext();
         }
     }
 }
